4d96b77c401049e93afbc6cf8fd37845491132af
Wilhlem Van Der Walt <va3093@gmail.com>
2023-01-01 21:49:42 +0200

    Update mod.ts

--- src/types/mod.ts
+++ src/types/mod.ts
@@ -2,3 +2,4 @@ export { Any } from "./any.ts";
 export { Numeric } from "./numeric.ts";
 export { Choice } from "./choice.ts";
 export { Alphabetical } from "./alphabetical.ts";
+export { Checklist } from "./checklist.ts";
